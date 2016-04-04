00ad96e222d3924942938d4212fc2c83c513ca5f
hugo <hugoby.scut@gmail.com>
2016-04-04 15:20:16 +0800

    GUI for searching or creating key for the users.

--- src/keystore/KeyStoreGUI.java
+++ src/keystore/KeyStoreGUI.java
@@ -1,13 +1,14 @@
 package keystore;
 
+
 import org.apache.commons.configuration.PropertiesConfiguration;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.security.KeyPair;
 import java.util.HashMap;
 
@@ -156,7 +157,5 @@ public class KeyStoreGUI extends JFrame implements ActionListener{
 
       public static void main(String args[]){
            new KeyStoreGUI();
-
       }
-
 }
